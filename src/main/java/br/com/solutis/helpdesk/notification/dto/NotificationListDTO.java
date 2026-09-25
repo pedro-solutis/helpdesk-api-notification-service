91cc34fa18e7ee181a108955d24d5385d5fdf844
Pedro Paulo Silva Santana Ferreira <pedro.ferreira@solutis.com.br>
2026-09-25 01:03:46 -0300

    feat(dto) add more field on ticketListDto

--- src/main/java/br/com/solutis/helpdesk/notification/dto/NotificationListDTO.java
+++ src/main/java/br/com/solutis/helpdesk/notification/dto/NotificationListDTO.java
@@ -1,14 +1,17 @@
 package br.com.solutis.helpdesk.notification.dto;
 
 import br.com.solutis.helpdesk.notification.model.Notification;
+import java.time.LocalDateTime;
 
 public record NotificationListDTO(
     Long id,
-    String type, 
+    String type,
     String message,
-    Long ticketId
+    Long ticketId,
+    boolean read,
+    LocalDateTime createdAt
 ) {
     public NotificationListDTO(Notification notification){
-        this(notification.getId(), notification.getType(), notification.getMessage(), notification.getTicketId());
+        this(notification.getId(), notification.getType(), notification.getMessage(), notification.getTicketId(), notification.isRead(), notification.getCreatedAt());
     }
 }
